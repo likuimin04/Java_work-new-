e9b4b3f00ed5d3dd238314e6e962a083fe0e9753
likuimin04 <likuimin04@outlook.com>
2020-11-30 15:31:48 +0900

    20.11.30 오후 15:31 추가됨

--- Step13_UtilClass/src/test/main/MainClass05.java
+++ Step13_UtilClass/src/test/main/MainClass05.java
@@ -19,12 +19,20 @@ public class MainClass05 {
 		 *  4...
 		 */
 		
+		// 여러개의 문자열의 참조값을 저장하기 위해 생성한 객체
 		List<String> mem=new ArrayList<>();
+		// 5 번 반복을 수행할 for 문 구성
+		Scanner scan=new Scanner(System.in);  // for문안에 생성을 하면 한번 사용하고 버리게 되기 때문에 여러번 사용할려면 for문 밖에 생성을 해야한다.
 		for(int i=0; i<5; i++) {
-			Scanner scan=new Scanner(System.in);
+			// 키보드로 부터 문자열을 입력 받기 위해 생성한 객체
 			System.out.println("친구 이름을 작성: ");
 			String names=scan.nextLine();
 			mem.add(names);
+		}
+		// 반복문을 돌면서 mem 에 저장된 내용을 정해진 형식으로 콘솔창에 출력하기
+		for(int i=0; i<5; i++) {
+			// i 번째 저장된 아이템을 읽어와서
+			String tmp=mem.get(i);
 			System.out.println(i+" 번째 친구 이름: "+mem.get(i));
 		}
 	}
