7e3431215cd57964821df3bd2c03f36f013cd4d4
likuimin04 <likuimin04@outlook.com>
2020-12-09 17:40:33 +0900

    20.12.09 오후 17:40 추가됨

---
+++ Step18_JDBC/src/test/main/MainClass11.java
@@ -0,0 +1,30 @@
+package test.main;
+
+import test.member.dao.MemberDao;
+import test.member.dto.MemberDto;
+
+public class MainClass11 {
+	public static void main(String[] args) {
+		/*
+		 *  아래는 member 테이블에 저장된 회원의 번호라고 가정을 하자
+		 *  
+		 *  그런데 저 번호의 회원이 존재하는지는 확실치 않다
+		 *  
+		 *  만일 존재한다면 해당 회원의 정보를 콘솔창에 출력하고
+		 *  
+		 *  존재하지 않는다면 해당 회원은 존재하지 않는다고 콘솔창에
+		 * 
+		 *  출력하는 프로그래밍을 해 보시오.
+		 *  
+		 *  단) MemberDao 객체를 활용해서...
+		 */
+		int num=807;
+		MemberDao dao=new MemberDao();
+		MemberDto dto=dao.select(num);
+		if(dto==null) {
+			System.out.println(num+"번 회원의 정보는 존재하지 않습니다.");
+		}else {
+			System.out.println(dto.getNum()+"|"+dto.getName()+"|"+dto.getAddr());;
+		}
+	}
+}
--- Step18_JDBC/src/test/member/dao/MemberDao.java
+++ Step18_JDBC/src/test/member/dao/MemberDao.java
@@ -2,6 +2,9 @@ package test.member.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import test.member.dto.MemberDto;
 import test.util.DBConnect;
@@ -14,9 +17,82 @@ import test.util.DBConnect;
  *  delete 
  *  작업을 할 객체를 생성할 클래스 설계하기 
  *  
+ *  - 필요한 객체를 담을 지역변수를 선언하는 위치도 중요하다.
+ *  - 필요한 객체를 생성하는 위치도 중요하다.
  */
 public class MemberDao {
-
+	// 모든 회원의 정보를 SLELECT 해서 리턴하는 메소드
+	public List<MemberDto> selectAll(){
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		List<MemberDto> list=new ArrayList<>();
+		try {
+			conn=new DBConnect().getConn();
+			// 실행할 SELECT문 작성
+			String sql="SELECT num,name,addr"
+					+ " FROM member"
+					+ "ORDER BY num ASC";
+			pstmt=conn.prepareStatement(sql);
+			// ? 에 바인딩 할게 있으면 하고 아님 말고
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+				MemberDto dto=new MemberDto();
+				dto.setNum(rs.getInt("num"));
+				dto.setName(rs.getString("name"));
+				dto.setAddr(rs.getString("addr"));
+				// 새로 생성한 MemberDto 객체의 참조값을 ArrayList 객체에 누적시킨다.
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {}
+		}
+		return list;
+	}
+	
+	//회원 한명의 정보를 SELECT 해서 리턴하는 메소드
+	// SELECT는 결과가 있기 때문에 ResultSet에 결과 값을 담아야한다.
+	public MemberDto select(int num) {
+		
+		// 회원 한명의 정보를 담고 있는 MemberDto 객체를 담을 지역변수 만들기
+		MemberDto dto=null;
+		// 필요한 객체의 참조값을 담을 지역변수 만들기
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try {
+			conn=new DBConnect().getConn();
+			String sql="SELECT name,addr FROM memeber"
+					+ " WHERE num=?";
+			pstmt=conn.prepareStatement(sql);
+			// ? 값에 바인딩 하기
+			pstmt.setInt(1, num);
+			// SELECT 문 수행하고 결과를 ResultSet 으로 받기
+			rs=pstmt.executeQuery();
+			if(rs.next()) { // 만일 select 된 row 가 있다면
+				// 결과를 MemberDto 객체를 생성해서 담는다.
+				dto=new MemberDto();
+				dto.setNum(num);
+				dto.setName(rs.getString("name"));
+				dto.setAddr(rs.getString("addr"));
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {}
+		}
+		
+		return dto;
+	}
 	// 1. 회원 한명의 정보를 저장하는 메소드를 만들어 보세요.
 	// 메소드명 : insert
 	// 리턴 type : 알아서
