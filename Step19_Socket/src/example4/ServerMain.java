ac394e757894d85680a908ba01f488f64678a84f
likuimin04 <likuimin04@outlook.com>
2020-12-14 10:01:38 +0900

    20.12.14 오전10:01 추가됨

--- Step19_Socket/src/example4/ServerMain.java
+++ Step19_Socket/src/example4/ServerMain.java
@@ -1,8 +1,11 @@
 package example4;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -30,6 +33,15 @@ public class ServerMain {
 				BufferedReader br=new BufferedReader(isr);
 				String msg=br.readLine();
 				System.out.println("클라이언트가 전송한 문자열:"+msg);
+				
+				//클라이언트에게 출력할수 있는 객체의 참조값
+				OutputStream os=socket.getOutputStream();
+				OutputStreamWriter osw=new OutputStreamWriter(os);
+				BufferedWriter bw=new BufferedWriter(osw);
+				bw.write("안녕 클라이언트야 난 서버야~");
+				bw.newLine();//개행기호를 메소드를 이용해서 출력할수 있다. 
+				bw.flush();
+				bw.close();
 				socket.close();
 			}
 		}catch(Exception e) {
