9090cdaf06b83488433296e8044569502ceab39a
likuimin04 <likuimin04@outlook.com>
2020-12-14 11:00:39 +0900

    20.12.14 오전11:00 추가됨

--- Step19_Socket/src/example5/ServerMain.java
+++ Step19_Socket/src/example5/ServerMain.java
@@ -23,69 +23,51 @@ public class ServerMain {
 			 */
 			while(true) {
 				System.out.println("클라이언트의 Socket 접속을 기다립니다...");
+				//1. 클라이언트가 접속을 해오면 
 				Socket socket=serverSocket.accept();
-				System.out.println("클라이언트가 접속을 했습니다.");
-				String ip=socket.getInetAddress().getHostAddress();
-				System.out.println("접속한 클라이언트의 ip 주소:"+ip);
-				//방금 접속을 한 클라이언트로 부터 입력받을 객체의 참조값
-				InputStream is=socket.getInputStream();
-				InputStreamReader isr=new InputStreamReader(is);
-				BufferedReader br=new BufferedReader(isr);
-				String msg=br.readLine();
-				System.out.println("클라이언트가 전송한 문자열:"+msg);
-				
-				//클라이언트에게 출력할수 있는 객체의 참조값
-				OutputStream os=socket.getOutputStream();
-				OutputStreamWriter osw=new OutputStreamWriter(os);
-				BufferedWriter bw=new BufferedWriter(osw);
-				bw.write("안녕 클라이언트야 난 서버야~");
-				bw.newLine();//개행기호를 메소드를 이용해서 출력할수 있다. 
-				bw.flush();
-				bw.close();
-				socket.close();
+				//2. 새로운 스레드를 시작 시킨다. 
+				ServerThread t=new ServerThread(socket);
+				t.start();
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("ServerMain main 메소드가 종료 됩니다.");
 	}
-	// 스레드 클래스 설계
+	//스레드 클래스 설계
 	static class ServerThread extends Thread{
-		// 필드( 클라이언트와 연결된 Socket 객체의 참조값을 저장할 필드)
-		private Socket socket;
+		//필드 (클라이언트와 연결된 Socket 객체의 참조값을 저장할 필드)
+		private Socket socket; 
 		
-		// 생성자
+		//생성자
 		public ServerThread(Socket socket) {
-			// 생성자의 인자로 전달받은 Socket 객체의 참조값을 필드에 저장하기
+			//생성자의 인자로 전달받은 Socket 객체의 참조값을 필드에 저장하기 
 			this.socket=socket;
 		}
+		
 		@Override
 		public void run() {
 			try {
-				// 클라이언트가 전송하는 문자열을 읽어들일 객체
-				/* InputStream is=socket.getInputStream();
-				   InputStreamReader isr=new InputStreamReader(is);
-				   BufferedReader br=new BufferedReader(isr);
-				*/
-				
-				//윗 내용 한줄로 전환
+				//클라이언트가 전송하는 문자열을 읽어들일 객체 
 				BufferedReader br=
-						new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				// 클라이언트에게 문자열을 출력할 객체
+					new BufferedReader(
+							new InputStreamReader(socket.getInputStream()));
+				//클라이언트에게 문자열을 출력할 객체
 				BufferedWriter bw=
-						new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-				// 반복문 돌면서 클라이언트가 전송하는 문자열이 있는지 읽어와 본다.
+					new BufferedWriter(
+							new OutputStreamWriter(socket.getOutputStream()));
+				//반복문 돌면서 클라이언트가 전송하는 문자열이 있는지 읽어와 본다.
 				while(true) {
-					// 문자열 한줄이 전송 될 때 까지 블록킹
+					//문자열 한줄이 전송될때 까지 블록킹 되는 메소드 
+					String line=br.readLine();
+					
 				}
-				
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
 }
-
 
 
 
